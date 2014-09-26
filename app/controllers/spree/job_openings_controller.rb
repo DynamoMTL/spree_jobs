c1b972c1dd96e425fd73c0cce28fc058a97c0eee
Josh Nussbaum <joshnuss@gmail.com>
2014-09-25 20:43:08 -0400

    Allow empty job location in job_openings controller

--- app/controllers/spree/job_openings_controller.rb
+++ app/controllers/spree/job_openings_controller.rb
@@ -5,7 +5,7 @@ module Spree
 
     def index
       @job_openings = ::Spree::JobOpening.where(active: true).order('created_at desc')
-      @grouped_job_openings = @job_openings.sort_by{ |o| o.job_location.address }.group_by { |o| o.job_location.city }
+      @grouped_job_openings = @job_openings.sort_by{ |o| o.job_location.try(:address) }.group_by { |o| o.job_location.try(:city) }
     end
 
     def show
